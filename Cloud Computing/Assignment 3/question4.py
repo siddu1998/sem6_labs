3babab046fd64b15fbb2fc7f12d41e19948026da
Sai Siddartha Maram <smaram_be16@thapar.edu>
2019-01-28 15:43:53 +0530

    half

---
+++ Cloud Computing/Assignment 3/question4.py
@@ -0,0 +1,5 @@
+list_of_words=['sai','siddartha','maram']
+n=int(input("please enter the number"))
+for i in list_of_words:
+    if (len(i)>n):
+        print(i)
---
+++ Cloud Computing/Assignment 3/question5.py
@@ -0,0 +1,2 @@
+list_with_duplicates=[1,2,1,2]
+print(list(set(list_with_duplicates)))
